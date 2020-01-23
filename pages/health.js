d0d8047bc33352eb979453b87b9c390378bf3d7f
Nicolas Bovorasmy <nbovorasmy@octo.com>
2020-01-23 13:31:54 +0100

    feat: add healthcheck

---
+++ pages/health.js
@@ -0,0 +1,12 @@
+import React from "react";
+import fetch from "isomorphic-unfetch";
+import Layout from "../src/components/Layout";
+import { useFetchUser } from "../src/lib/user";
+import NewsCard from "../src/components/NewsCard";
+import TeamsList from "../src/components/TeamsList";
+
+export default function Home() {
+  return (
+      <div>It Works!</div>
+  )
+}
