d59d68e53d74f6896cbaf62a855512f9b0423334
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-01-24 14:20:34 +0100

    dynamic domain for feature branches

--- src/lib/auth0.js
+++ src/lib/auth0.js
@@ -1,12 +1,18 @@
 import { initAuth0 } from "@auth0/nextjs-auth0";
 import config from "../config";
 
+const redirectUri = `${
+  config.DOMAIN.includes("carnets") ? "https" : "http"
+}://${config.DOMAIN}/api/callback`;
+
+console.log("initAuth0", redirectUri);
+
 export default initAuth0({
   clientId: config.AUTH0_CLIENT_ID,
   clientSecret: config.AUTH0_CLIENT_SECRET,
   scope: config.AUTH0_SCOPE,
   domain: config.AUTH0_DOMAIN,
-  redirectUri: config.REDIRECT_URI,
+  redirectUri: redirectUri,
   postLogoutRedirectUri: config.POST_LOGOUT_REDIRECT_URI,
   session: {
     cookieSecret: config.SESSION_COOKIE_SECRET,
