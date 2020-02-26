8fc6f9492d664fbc982ca3bc038b689723ce5570
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-02-26 15:21:52 +0100

    display CI env variables to test

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,6 +5,12 @@ import Nav from "./Nav";
 
 import { UserProvider } from "../lib/user";
 
+const CI_COMMIT_BEFORE_SHA = process.env.CI_COMMIT_BEFORE_SHA;
+const CI_COMMIT_REF_SLUG = process.env.CI_COMMIT_REF_SLUG;
+const CI_COMMIT_REF_NAME = process.env.CI_COMMIT_REF_NAME;
+const CI_COMMIT_SHORT_SHA = process.env.CI_COMMIT_SHORT_SHA;
+const CI_COMMIT_SHA = process.env.CI_COMMIT_SHA;
+
 const Layout = ({ user, loading = false, children }) => (
   <UserProvider value={{ user, loading }}>
     <Head>
@@ -15,6 +21,13 @@ const Layout = ({ user, loading = false, children }) => (
 
     <div className="container" style={{ paddingTop: 40 }}>
       {children}
+    </div>
+    <div>
+      <div>CI_COMMIT_BEFORE_SHA: {CI_COMMIT_BEFORE_SHA}</div>
+      <div>CI_COMMIT_REF_SLUG: {CI_COMMIT_REF_SLUG}</div>
+      <div>CI_COMMIT_REF_NAME: {CI_COMMIT_REF_NAME}</div>
+      <div>CI_COMMIT_SHORT_SHA: {CI_COMMIT_SHORT_SHA}</div>
+      <div>CI_COMMIT_SHA: {CI_COMMIT_SHA}</div>
     </div>
   </UserProvider>
 );
