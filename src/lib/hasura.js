88fb194f355c2708c96d158ae512f95bfe61bc3b
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-03-03 01:06:47 +0100

    expose HASURA_URL

--- src/lib/hasura.js
+++ src/lib/hasura.js
@@ -3,6 +3,8 @@ import { GraphQLClient } from "graphql-request";
 const url = process.env.HASURA_URL;
 const secret = process.env.HASURA_SECRET;
 
+console.log("HASURA_URL", url);
+
 export const fetch = async (query, variables) => {
   const graphQLClient = new GraphQLClient(url, {
     headers: {
