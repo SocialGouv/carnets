efd5577a069aadea2fb350e42924450448ed8535
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-03-09 16:22:51 +0100

    fix container dimensions calculation for SVG chart

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -2,7 +2,6 @@ import auth0 from "../../src/lib/auth0";
 
 export default async function callback(req, res) {
   try {
-    console.log("Query state:", req.query.state);
     await auth0.handleCallback(req, res, {
       redirectTo: "/publish?id=" + req.query.state
     });
--- src/components/stat/PostsStat.js
+++ src/components/stat/PostsStat.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import Dimensions from "react-dimensions";
+// import Dimensions from "react-dimensions";
 import { VictoryChart, VictoryBar, VictoryAxis } from "victory";
 
-const PostsStat = ({ data, containerWidth }) => {
+import ContainerDimensions from "react-container-dimensions";
+
+const PostsStat = ({ data /*, containerWidth*/ }) => {
   const weeks = Array.from(Array(52), (e, i) => i + 1);
 
   const styles = {
@@ -22,27 +24,35 @@ const PostsStat = ({ data, containerWidth }) => {
   return (
     <Wrapper>
       <h4>Nombre de publications par semaine</h4>
-      <VictoryChart
-        height={200}
-        width={containerWidth || 0}
-        domainPadding={{ x: 20, y: 0 }}
-        padding={{ top: 20, bottom: 60, left: 40, right: 40 }}
-      >
-        <VictoryAxis
-          dependentAxis
-          style={styles.axis}
-          label="Publications"
-          tickFormat={t => Math.round(t)}
-        />
-        <VictoryAxis label="Semaines" tickValues={weeks} style={styles.axis} />
-        <VictoryBar
-          x="week"
-          y="count"
-          data={data}
-          barWidth={10}
-          style={styles.bar}
-        />
-      </VictoryChart>
+      <ContainerDimensions>
+        {({ width, height }) => (
+          <VictoryChart
+            width={width || 0}
+            height={height || 0}
+            domainPadding={{ x: 20, y: 0 }}
+            padding={{ top: 20, bottom: 60, left: 40, right: 40 }}
+          >
+            <VictoryAxis
+              dependentAxis
+              style={styles.axis}
+              label="Publications"
+              tickFormat={t => Math.round(t)}
+            />
+            <VictoryAxis
+              label="Semaines"
+              tickValues={weeks}
+              style={styles.axis}
+            />
+            <VictoryBar
+              x="week"
+              y="count"
+              data={data}
+              barWidth={10}
+              style={styles.bar}
+            />
+          </VictoryChart>
+        )}
+      </ContainerDimensions>
     </Wrapper>
   );
 };
@@ -50,11 +60,6 @@ const PostsStat = ({ data, containerWidth }) => {
 const Wrapper = styled.div`
   width: 100%;
   height: 200px;
-  /* background-color: pink; */
-
-  h4 {
-    /* margin-bottom: 20px; */
-  }
 `;
 
-export default Dimensions()(PostsStat);
+export default PostsStat;
