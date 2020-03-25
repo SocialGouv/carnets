a7ff36b3f192cd7679b0b3d8ba885ceba1f55d95
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-03-26 00:57:13 +0100

    add APP_BASE_URL env var

--- src/lib/auth0.js
+++ src/lib/auth0.js
@@ -1,22 +1,15 @@
 import { initAuth0 } from "@auth0/nextjs-auth0"
 
 const Auth0 = () => {
+  console.log("Auth0 - process.env.APP_BASE_URL:", process.env.APP_BASE_URL)
   console.log("Auth0 - process.env.AUTH0_DOMAIN:", process.env.AUTH0_DOMAIN)
   console.log(
     "Auth0 - process.env.AUTH0_CLIENT_ID:",
     process.env.AUTH0_CLIENT_ID
   )
   console.log(
-    "Auth0 - process.env.AUTH0_REDIRECT_URI:",
-    process.env.AUTH0_REDIRECT_URI
-  )
-  console.log(
     "Auth0 - process.env.AUTH0_CLIENT_SECRET:",
     process.env.AUTH0_CLIENT_SECRET
-  )
-  console.log(
-    "Auth0 - process.env.AUTH0_POST_LOGOUT_REDIRECT_URI:",
-    process.env.AUTH0_POST_LOGOUT_REDIRECT_URI
   )
   console.log(
     "Auth0 - process.env.AUTH0_SESSION_COOKIE_SECRET:",
@@ -27,9 +20,9 @@ const Auth0 = () => {
     scope: "openid profile",
     domain: process.env.AUTH0_DOMAIN,
     clientId: process.env.AUTH0_CLIENT_ID,
-    redirectUri: process.env.AUTH0_REDIRECT_URI,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    postLogoutRedirectUri: process.env.AUTH0_POST_LOGOUT_REDIRECT_URI,
+    postLogoutRedirectUri: process.env.APP_BASE_URL,
+    redirectUri: `${process.env.APP_BASE_URL}/api/auth0/callback`,
     session: { cookieSecret: process.env.AUTH0_SESSION_COOKIE_SECRET }
   })
 }
