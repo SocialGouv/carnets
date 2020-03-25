65a267970ab1589ab56349d1f06e190aeeba0562
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-03-25 11:16:54 +0100

    test HASURA_URL env var within code

--- src/lib/hasura.js
+++ src/lib/hasura.js
@@ -1,5 +1,7 @@
 import { GraphQLClient } from "graphql-request"
 
+console.log("process.env.HASURA_URL:", process.env.HASURA_URL)
+console.log("process.env.TEST_FOUR:", process.env.TEST_FOUR)
 const url = process.env.TEST_FOUR || process.env.HASURA_URL
 const secret = process.env.HASURA_GRAPHQL_ADMIN_SECRET
 
