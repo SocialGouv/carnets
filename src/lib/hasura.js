450d7d421dd2848a99b8424de976c2d3e57e88f2
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-03-25 11:38:53 +0100

    test env var combos

--- src/lib/hasura.js
+++ src/lib/hasura.js
@@ -1,7 +1,13 @@
 import { GraphQLClient } from "graphql-request"
 
-console.log("process.env.HASURA_URL:", process.env.HASURA_URL)
+console.log("process.env.TEST_ONE:", process.env.TEST_ONE)
+console.log("process.env.TEST_TWO:", process.env.TEST_TWO)
+console.log("process.env.TEST_THREE:", process.env.TEST_THREE)
 console.log("process.env.TEST_FOUR:", process.env.TEST_FOUR)
+console.log("process.env.TEST_FIVE:", process.env.TEST_FIVE)
+console.log("process.env.TEST_SIX:", process.env.TEST_SIX)
+console.log("process.env.TEST_SEVEN:", process.env.TEST_SEVEN)
+console.log("process.env.HASURA_URL:", process.env.HASURA_URL)
 const url = process.env.TEST_FOUR || process.env.HASURA_URL
 const secret = process.env.HASURA_GRAPHQL_ADMIN_SECRET
 
