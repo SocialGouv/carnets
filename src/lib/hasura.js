24bee2fbfe1caa84bfedc33bf5e8e3b132babf56
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-03-25 23:01:04 +0100

    remove useless env vars from build

--- src/lib/hasura.js
+++ src/lib/hasura.js
@@ -1,16 +1,17 @@
 import { GraphQLClient } from "graphql-request"
 
-console.log("process.env.TEST_ONE:", process.env.TEST_ONE)
-console.log("process.env.TEST_TWO:", process.env.TEST_TWO)
-console.log("process.env.TEST_THREE:", process.env.TEST_THREE)
-console.log("process.env.TEST_FOUR:", process.env.TEST_FOUR)
-console.log("process.env.TEST_FIVE:", process.env.TEST_FIVE)
-console.log("process.env.TEST_SIX:", process.env.TEST_SIX)
-console.log("process.env.TEST_SEVEN:", process.env.TEST_SEVEN)
-console.log("process.env.TEST_HEIGHT:", process.env.TEST_HEIGHT)
-console.log("process.env.TEST_NINE:", process.env.TEST_NINE)
-console.log("process.env.HASURA_URL:", process.env.HASURA_URL)
-const url = process.env.TEST_FOUR || process.env.HASURA_URL
+console.log("hasura - process.env.TEST_ONE:", process.env.TEST_ONE)
+console.log("hasura - process.env.TEST_TWO:", process.env.TEST_TWO)
+console.log("hasura - process.env.TEST_THREE:", process.env.TEST_THREE)
+console.log("hasura - process.env.TEST_FOUR:", process.env.TEST_FOUR)
+console.log("hasura - process.env.TEST_FIVE:", process.env.TEST_FIVE)
+console.log("hasura - process.env.TEST_SIX:", process.env.TEST_SIX)
+console.log("hasura - process.env.TEST_SEVEN:", process.env.TEST_SEVEN)
+console.log("hasura - process.env.TEST_HEIGHT:", process.env.TEST_HEIGHT)
+console.log("hasura - process.env.TEST_NINE:", process.env.TEST_NINE)
+console.log("hasura - process.env.HASURA_URL:", process.env.HASURA_URL)
+
+const url = process.env.HASURA_URL
 const secret = process.env.HASURA_GRAPHQL_ADMIN_SECRET
 
 export const fetch = async (query, variables) => {
