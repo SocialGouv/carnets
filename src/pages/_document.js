624854f2ae334eedd33916ff90123991b9c0aa43
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-04-30 21:09:20 +0200

    feat(matomo): Add matomo head script.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,6 +12,30 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" href="/favicon.png" />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                var _paq = window._paq || []
+                /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+                _paq.push(["trackPageView"])
+                _paq.push(["enableLinkTracking"])
+                ;(function () {
+                  var u = "https://matomo.fabrique.social.gouv.fr/"
+                  _paq.push(["setTrackerUrl", u + "matomo.php"])
+                  _paq.push(["setSiteId", "24"])
+                  var d = document,
+                    g = d.createElement("script"),
+                    s = d.getElementsByTagName("script")[0]
+                  g.type = "text/javascript"
+                  g.async = true
+                  g.defer = true
+                  g.src = u + "matomo.js"
+                  s.parentNode.insertBefore(g, s)
+                })()
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
