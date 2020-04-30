19727f4a295d725d91c0bf35909178affa0c13e6
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-05-01 00:37:56 +0200

    feat(sentry): Add Sentry.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 const { version } = require("./package.json")
+const withSourceMaps = require("@zeit/next-source-maps")
 
-module.exports = {
+// module.exports = {
+//   env: {
+//     APP_VERSION: version,
+//     APP_REVISION: process.env.CI_COMMIT_SHA || "dev"
+//   }
+// }
+
+module.exports = withSourceMaps({
   env: {
     APP_VERSION: version,
-    APP_REVISION: process.env.CI_COMMIT_SHA || "dev"
-  }
-}
+    APP_REVISION: process.env.CI_COMMIT_SHA || "dev",
+  },
+  webpack(config, _options) {
+    return config
+  },
+})
