ae326bdceb7e50a2d46f39902ec573b6ca2bdf4f
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-04-30 14:05:28 +0200

    feat(hasura): Add Hasura authentication. (#97)
    Customize pipeline.
    
    Log access token.
    
    Add missing AUTH0_AUDIENCE env var.
    
    Remove access token log.

--- src/lib/auth0.js
+++ src/lib/auth0.js
@@ -4,11 +4,17 @@ const Auth0 = () => {
   return initAuth0({
     scope: "openid profile",
     domain: process.env.AUTH0_DOMAIN,
+    audience: process.env.AUTH0_AUDIENCE,
     clientId: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     postLogoutRedirectUri: process.env.APP_BASE_URL,
     redirectUri: `${process.env.APP_BASE_URL}/api/auth0/callback`,
-    session: { cookieSecret: process.env.AUTH0_SESSION_COOKIE_SECRET },
+    session: {
+      storeIdToken: true,
+      storeAccessToken: true,
+      storeRefreshToken: true,
+      cookieSecret: process.env.AUTH0_SESSION_COOKIE_SECRET,
+    },
   })
 }
 
