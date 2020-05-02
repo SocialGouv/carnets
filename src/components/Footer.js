d6a787d3bfffcccc688322e53269e3c402941d12
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-05-02 17:12:01 +0200

    Get revision on run, second attempt.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,32 +2,37 @@ import React from "react"
 
 const version = process.env.APP_VERSION
 // const revision = process.env.APP_REVISION
-const revision = process.env.CI_COMMIT_SHA || "dev"
+console.log("CI_COMMIT_SHA 1", process.env.CI_COMMIT_SHA)
 
-const Footer = () => (
-  <div className="footer">
-    <div className="info">
-      Pour en savoir plus sur l&apos;utilisation de <b>Carnets</b>, visitez le{" "}
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/SocialGouv/www/wiki/Inscrire-son-%C3%A9quipe-au-standup"
-      >
-        wiki
-      </a>
-      .
+const Footer = () => {
+  console.log("CI_COMMIT_SHA 2", process.env.CI_COMMIT_SHA)
+  const revision = process.env.CI_COMMIT_SHA || "dev"
+
+  return (
+    <div className="footer">
+      <div className="info">
+        Pour en savoir plus sur l&apos;utilisation de <b>Carnets</b>, visitez le{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/SocialGouv/www/wiki/Inscrire-son-%C3%A9quipe-au-standup"
+        >
+          wiki
+        </a>
+        .
+      </div>
+      <div className="version">
+        version {version}{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`https://github.com/SocialGouv/carnets/tree/${revision}`}
+        >
+          ({revision.substring(0, 6)})
+        </a>
+      </div>
     </div>
-    <div className="version">
-      version {version}{" "}
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href={`https://github.com/SocialGouv/carnets/tree/${revision}`}
-      >
-        ({(revision || "").substring(0, 6)})
-      </a>
-    </div>
-  </div>
-)
+  )
+}
 
 export default Footer
