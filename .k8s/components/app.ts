d4817c294348d52e9ca904a91d5145adb3d480e5
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-08-26 23:39:15 +0200

    Kosko3 (#189)
    * Upgrade pipe.
    
    * Upgrade pipe, again.
    
    * Upgrade pipe, again and again.
    
    * Update app.configmap.yaml
    
    * Update app.configmap.yaml
    
    * Upgrade pipe, again and again and again.
    
    * Upgrade pipe, again and again and again and again.
    
    * Upgrade pipe, again and again and again and again and again.
    
    * Add APP_BASE_URL env variable.
    
    * Remove useless env var.
    
    * Add preprod k8s config.
    
    Co-authored-by: Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>

---
+++ .k8s/components/app.ts
@@ -0,0 +1,47 @@
+import env from "@kosko/env"
+import { ok } from "assert"
+import { EnvVar } from "kubernetes-models/v1/EnvVar"
+import { Deployment } from "kubernetes-models/apps/v1/Deployment"
+import { Ingress } from "kubernetes-models/api/networking/v1beta1/Ingress"
+
+import { IoK8sApiAppsV1Deployment } from "kubernetes-models/_definitions/IoK8sApiAppsV1Deployment"
+import { IoK8sApiNetworkingV1beta1Ingress } from "kubernetes-models/_definitions/IoK8sApiNetworkingV1beta1Ingress"
+
+import { create } from "@socialgouv/kosko-charts/components/app"
+import { addEnv } from "@socialgouv/kosko-charts/utils/addEnv"
+import { getManifestByKind } from "@socialgouv/kosko-charts/utils/getManifestByKind"
+import { getIngressHost } from "@socialgouv/kosko-charts/utils/getIngressHost"
+
+const manifests = create("app", {
+  env,
+  config: { containerPort: 3000 },
+  deployment: {
+    container: {
+      resources: {
+        requests: {
+          cpu: "5m",
+          memory: "128Mi",
+        },
+        limits: {
+          cpu: "1000m",
+          memory: "256Mi",
+        },
+      },
+    },
+  },
+})
+
+/* pass dynamic deployment URL as env var to the container */
+//@ts-expect-error
+const deployment = getManifestByKind(manifests, Deployment) as Deployment
+
+ok(deployment)
+
+const frontendUrl = new EnvVar({
+  name: "APP_BASE_URL",
+  value: `https://${getIngressHost(manifests)}`,
+})
+
+addEnv({ deployment, data: frontendUrl })
+
+export default manifests
