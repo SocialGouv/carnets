aff199dfa36e4b082d22bcf8e94d5f1b3cc975de
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-09-16 22:26:42 +0200

    fix: Fix team file upload.

--- src/pages/api/teams/[team]/files.js
+++ src/pages/api/teams/[team]/files.js
@@ -4,21 +4,11 @@ import formidable from "formidable"
 
 export const config = { api: { bodyParser: false } }
 
-function msleep(n) {
-  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n)
-}
-function sleep(n) {
-  msleep(n * 1000)
-}
-
 const getFiles = (req) => {
   const form = formidable({ multiples: true })
-  sleep(3)
 
   return new Promise((resolve, reject) => {
     try {
-      sleep(3)
-      console.log("parse form")
       form.parse(req, (err, fields, files) => resolve(files))
     } catch (error) {
       console.log(error)
@@ -38,16 +28,12 @@ export default async (req, res) => {
       res.json(files)
     } else if (req.method === "POST") {
       const [, token] = await getInfo(req, res)
-      console.log("--> 1 token", token)
       const files = await getFiles(req, team, token)
-      console.log("--> 2 files", files)
       const uploadedFiles = await uploadFiles(files, team)
-      console.log("--> 3 uploadedFiles", uploadedFiles)
       uploadedFiles.forEach((file) => {
         file.team_slug = team
       })
       const registeredFiles = await registerFiles(uploadedFiles, token)
-      console.log("--> 4 registeredFiles", registeredFiles)
       return res.json(registeredFiles)
     } else {
       res.status(405)
