8edb47ea0462caf2eb92dbccdcd9f4ee0cdd4840
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-10-02 15:53:21 +0200

    fix: Reset Hasura config.

--- .k8s/components/hasura.ts
+++ .k8s/components/hasura.ts
@@ -4,37 +4,9 @@ import { create } from "@socialgouv/kosko-charts/components/hasura"
 
 const manifests = create({
   env,
-  config: { containerPort: 3000 },
-  deployment: {
-    container: {
-      resources: {
-        requests: {
-          cpu: "50m",
-          memory: "256Mi",
-        },
-        limits: {
-          cpu: "500m",
-          memory: "256Mi",
-        },
-      },
-      livenessProbe: {
-        httpGet: {
-          path: "/healthz",
-          port: "http",
-        },
-        initialDelaySeconds: 60,
-        periodSeconds: 20,
-      },
-      readinessProbe: {
-        httpGet: {
-          path: "/healthz",
-          port: "http",
-        },
-        initialDelaySeconds: 60,
-        periodSeconds: 20,
-      },
-    },
-  },
+  // config: {
+  //   ingress: false,
+  // },
 })
 
 export default manifests
