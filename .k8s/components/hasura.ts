5c13ac07c51a6688315b2a915466fcf99cf8a0d3
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-10-02 16:12:46 +0200

    fix: Retry Hasura config.

--- .k8s/components/hasura.ts
+++ .k8s/components/hasura.ts
@@ -4,9 +4,37 @@ import { create } from "@socialgouv/kosko-charts/components/hasura"
 
 const manifests = create({
   env,
-  // config: {
-  //   ingress: false,
-  // },
+  config: { containerPort: 3000 },
+  deployment: {
+    container: {
+      resources: {
+        requests: {
+          cpu: "50m",
+          memory: "256Mi",
+        },
+        limits: {
+          cpu: "500m",
+          memory: "256Mi",
+        },
+      },
+      livenessProbe: {
+        httpGet: {
+          path: "/healthz",
+          port: "http",
+        },
+        initialDelaySeconds: 60,
+        periodSeconds: 20,
+      },
+      readinessProbe: {
+        httpGet: {
+          path: "/healthz",
+          port: "http",
+        },
+        initialDelaySeconds: 60,
+        periodSeconds: 20,
+      },
+    },
+  },
 })
 
 export default manifests
