c30fc80dbc665ffac1fd448d7523a2f615281e2c
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-11-16 12:33:16 +0100

    fix: 3 states followup statuses. (#257)
    * fix: 3 states followup statuses.
    
    * style: Followup status edition button.

--- src/components/followup/items/Status.js
+++ src/components/followup/items/Status.js
@@ -1,25 +1,59 @@
 import { Field } from "formik"
-import React from "react"
-import { CheckCircle, XCircle } from "react-feather"
+import React, { useEffect, useState } from "react"
+import { CheckCircle, Circle, XCircle } from "react-feather"
 
-const Status = ({ status }) => (
+const statuses = [
+  { color: "green", icon: CheckCircle, value: "ok" },
+  { color: "crimson", icon: XCircle, value: "ko" },
+  { color: "grey", icon: Circle, value: "na" },
+]
+
+const Status = ({ status: currentStatus }) => (
   <>
-    {status === false && <XCircle color="crimson" size={16} />}
-    {status === true && <CheckCircle color="green" size={16} />}
-    {/* {status === null && <CheckCircle color="#dfe2e5" size={16} />} */}
+    {statuses.map((status, i) => (
+      <React.Fragment key={i}>
+        {status.value === currentStatus && (
+          <status.icon color={status.color} size={16} />
+        )}
+      </React.Fragment>
+    ))}
   </>
 )
+
+const StatusField = ({ field: { name, value }, form: { setFieldValue } }) => {
+  const [currentValue, setCurrentValue] = useState(value)
+
+  const next = (currentValue) => {
+    const currentIndex = statuses.findIndex(
+      (status) => status.value === currentValue
+    )
+    const currentStatus =
+      statuses[currentIndex < statuses.length - 1 ? currentIndex + 1 : 0]
+
+    setCurrentValue(currentStatus.value)
+  }
+
+  useEffect(() => {
+    setFieldValue(name, currentValue)
+  }, [currentValue, name, setFieldValue])
+
+  return (
+    <button type="button" onClick={() => next(currentValue)}>
+      <Status status={currentValue} />
+    </button>
+  )
+}
 
 const Write = ({ item }) => (
   <>
     <div>{item.label}</div>
-    <Field type="checkbox" name={`${item.fieldIndex}.status`} />
+    <Field name={`${item.fieldIndex}.status`} component={StatusField} />
   </>
 )
 
 const Read = ({ item }) => (
   <>
-    <div className="label">{item.label}</div>
+    <div className={`label ${item.status}`}>{item.label}</div>
     <Status status={item.status} />
   </>
 )
