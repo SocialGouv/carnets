789cc040486deea078910872b882f7fbd8d47970
Douglas DUTEIL <douglasduteil@gmail.com>
2020-11-25 17:16:54 +0100

    draft(k8s): test @socialgouv/kosko-chart@4.x-alpha

--- .k8s/components/hasura.ts
+++ .k8s/components/hasura.ts
@@ -4,9 +4,6 @@ import { create } from "@socialgouv/kosko-charts/components/hasura"
 
 const manifests = create({
   env,
-  // config: {
-  //   ingress: false,
-  // },
 })
 
 export default manifests
