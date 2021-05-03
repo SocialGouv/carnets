880ab8cbc30d540233004031d06d981554f5b3fa
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-05-03 23:31:21 +0200

    feat(posts): dont edit after 4 days

--- src/components/posts/Header.js
+++ src/components/posts/Header.js
@@ -6,12 +6,15 @@ import React from "react"
 import Delete from "./Delete"
 import Edit from "./Edit"
 
+const MAX_EDITABLE_POST_AGE = 1000 * 60 * 60 * 24 * 4 // 4 days
+const isOldPost = post => new Date().getTime() - new Date(post.created_at).getTime() > MAX_EDITABLE_POST_AGE;
+
 const Header = ({ post }) => {
   const { team_slug } = post
   const user = useUser() || {}
   const teams = useTeams() || {}
   const team = teams.find((team) => team.slug === post.team_slug)
-  const isAllowed = team && (user.isAdmin || user.teams?.includes(team_slug))
+  const isAllowed = team && !isOldPost(post) && (user.isAdmin || user.teams?.includes(team_slug))
 
   return (
     <div className="header">
