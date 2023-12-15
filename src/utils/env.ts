f746246a1608f7146245b5ae157df2d322464e41
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-12-15 05:05:16 +0100

    fix: NEXTAUTH_SECRET

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -10,37 +10,4 @@ export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET as string;
 // Github auth provider
 export const GITHUB_ID = process.env.GITHUB_ID as string;
 export const GITHUB_SECRET = process.env.GITHUB_SECRET as string;
-
-// Services tokens
-// export const MATOMO_API_TOKEN = process.env.MATOMO_API_TOKEN as string;
-// export const SENTRY_API_TOKEN = process.env.SENTRY_API_TOKEN as string;
-// export const OVH_APP_KEY = process.env.OVH_APP_KEY as string;
-// export const OVH_APP_SECRET = process.env.OVH_APP_SECRET as string;
-// export const OVH_CONSUMER_KEY = process.env.OVH_CONSUMER_KEY as string;
-// export const OVH_SERVICE_NAME = process.env.OVH_SERVICE_NAME as string;
-// export const ZAMMAD_API_TOKEN = process.env.ZAMMAD_API_TOKEN as string;
-// export const NEXTCLOUD_API_LOGIN = process.env.NEXTCLOUD_API_LOGIN as string;
-// export const NEXTCLOUD_API_SECRET = process.env.NEXTCLOUD_API_SECRET as string;
-// export const MATTERMOST_API_TOKEN = process.env.MATTERMOST_API_TOKEN as string;
-// export const GITHUB_API_TOKEN = process.env.GITHUB_API_TOKEN as string;
-
-// // Github auth provider
-// export const GITHUB_ID = process.env.GITHUB_ID as string;
-// export const GITHUB_SECRET = process.env.GITHUB_SECRET as string;
-
-// export const NEXTAUTH_URL = process.env.NEXTAUTH_URL as string;
-// export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET as string;
-
-// // Tipimail API credentials
-// export const TIPIMAIL_API_KEY = process.env.TIPIMAIL_API_KEY as string;
-// export const TIPIMAIL_API_USER = process.env.TIPIMAIL_API_USER as string;
-
-// export const ONBOARDING_NOTIFICATION_EMAILS = process.env
-//   .ONBOARDING_NOTIFICATION_EMAILS as string;
-
-// export const NEXT_PUBLIC_HASURA_URL = process.env
-//   .NEXT_PUBLIC_HASURA_URL as string;
-
-// // Mattermost review alert
-// export const MATTERMOST_ALERT_CHANNEL_ID = process.env
-//   .MATTERMOST_ALERT_CHANNEL_ID as string;
+export const NEXTAUTH_URL = process.env.NEXTAUTH_URL as string;
