4b8eac81605ac4725e4fe9cf6d3029dcdf1a86ac
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2024-01-26 19:14:43 +0100

    chore: add lcov report

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      provider: "v8",
+      reporter: ["lcov"],
+    },
     environment: "jsdom",
     setupFiles: "vitest.setup.ts",
     alias: {
