f6a84ae04b566d189b49dfb3305e69e4613780bd
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2024-01-30 07:08:35 +0100

    chore: mock server call

--- __tests__/teams.test.jsx
+++ __tests__/teams.test.jsx
@@ -2,6 +2,21 @@ import { vi, expect, test } from "vitest";
 import { render } from "@testing-library/react";
 import Component from "../src/components/teams.tsx";
 
+vi.mock("@/actions/get-teams", async (importOriginal) => {
+  const actual = await importOriginal();
+
+  return {
+    ...actual,
+    default: () => [
+      {
+        name: "domifa",
+        slug: "domifa",
+        avatarUrl: "https://avatar.domifa.com",
+      },
+    ],
+  };
+});
+
 test("Component", async () => {
   const { container } = render(await Component({ selectedTeamSlug: "domifa" }));
   expect(container).toMatchSnapshot();
