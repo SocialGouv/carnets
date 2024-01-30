287e23a0d265ad4e9151d59eac1fdf9ae308107e
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2024-01-30 07:46:47 +0100

    chore: add layout test

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -11,3 +11,17 @@ vi.mock("react", async () => {
     cache: testCache,
   };
 });
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
