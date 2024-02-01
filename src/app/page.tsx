bc1b683124b37e1465fe4b083ed5f494700bf71f
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2024-02-01 02:54:44 +0100

    fix: padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
       <aside className="flex-initial w-80">
         <Teams />
       </aside>
-      <main className="flex-1 py-12 ">
+      <main className="flex-1 pt-12 ">
         <Posts posts={posts} />
       </main>
     </>
