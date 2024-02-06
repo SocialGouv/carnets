2c565507ebb59b8f5ea8cd84fb17d6302647bb7a
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2024-02-06 13:29:49 +0100

    fix: relocate deletion modal

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,16 +1,11 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
 import { format } from "date-fns";
 import remarkGfm from "remark-gfm";
 import Markdown from "react-markdown";
-import { useRouter } from "next/navigation";
-import deletePost from "@/actions/delete-post";
 import Button from "@codegouvfr/react-dsfr/Button";
 import KPIs, { type KPI } from "@/components/common/kpis";
-import SpinnerIcon from "@/components/icons/spinner-icon";
-import { createModal } from "@codegouvfr/react-dsfr/Modal";
 import Mood, { type Mood as MoodType } from "@/components/common/mood";
 import TabPanel, { Tab, Tabs, Panels } from "@/components/common/tab-panel";
 
@@ -27,129 +22,86 @@ export interface Post {
   priorities: string;
 }
 
-const modal = createModal({
-  id: "delete-post",
-  isOpenedByDefault: false,
-});
-
 export default function Post({
   data,
   editable,
   hideLogo,
+  onDeletion,
 }: {
   data: Post;
   editable: boolean;
   hideLogo: boolean;
+  onDeletion: (id?: string) => void;
 }) {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
-
-  async function handleDeletion() {
-    setIsLoading(true);
-    await deletePost(data.id);
-    router.refresh();
-    setIsLoading(false);
-    modal.close();
-  }
-
   return (
-    <>
-      <modal.Component title="Suppression">
-        <div className="relative">
-          <div className={`${isLoading ? "invisible" : ""}`}>
-            <p className="my-8">
-              Êtes-vous sur de vouloir supprimer cette publication ?
-            </p>
+    <article className="post">
+      <div className="header">
+        <div className="avatar">
+          {hideLogo ? (
+            <Mood mood={data.mood} />
+          ) : (
+            <Image
+              width={48}
+              height={48}
+              alt="Picture of the team"
+              src={data.team?.avatarUrl || ""}
+            />
+          )}
+        </div>
+        <div className="text">{data.team?.name}</div>
+        {!hideLogo && <Mood mood={data.mood} />}
+      </div>
+      <KPIs kpis={data.kpis?.slice(0, 3) || []}></KPIs>
+      <TabPanel>
+        <Tabs>
+          <Tab disabled={!data.priorities.length}>Priorités</Tab>
+          <Tab disabled={!data.needs.length}>Besoins</Tab>
+          <Tab disabled={!data.term.length}>Échéances</Tab>
+        </Tabs>
+        <Panels>
+          <div className="markdown-body">
+            <Markdown remarkPlugins={[remarkGfm]}>{data.priorities}</Markdown>
+          </div>
+          <div className="markdown-body">
+            <Markdown remarkPlugins={[remarkGfm]}>{data.needs}</Markdown>
+          </div>
+          <div className="markdown-body">
+            <Markdown remarkPlugins={[remarkGfm]}>{data.term}</Markdown>
+          </div>
+        </Panels>
+      </TabPanel>
+      <div className="footer">
+        <div className="info">
+          {data.created_at && format(new Date(data.created_at), "dd/MM/yyyy")}
+          {" - "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="mr-2"
+            href={`https://github.com/${data.author}`}
+          >
+            {data.author}
+          </a>
+        </div>
+        {editable && (
+          <div className="actions divide-x">
+            <Button
+              title="editer"
+              iconId="fr-icon-draft-line"
+              priority="tertiary no outline"
+              linkProps={{
+                href: `/${data.team_slug}/publish/${data.id}`,
+              }}
+            />
             <Button
               title="supprimer"
-              priority="primary"
-              onClick={handleDeletion}
               iconId="fr-icon-delete-line"
-              // TODO: make this button redish
-              // className="bg-[#c9191e] dark:bg-[#f95c5e] dark:text-[#2b1919] text-[#fef4f4] hover:dark:text-[#f95c5e] hover:dark:bg-[#f95c5e] hover:text-[#c9191e] hover:bg-[#c9191e]"
-            >
-              Supprimer
-            </Button>
+              priority="tertiary no outline"
+              onClick={() => onDeletion(data.id)}
+            />
           </div>
-          {isLoading && (
-            <div className="absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center">
-              <div className="animate-spin w-24 h-24">
-                <SpinnerIcon />
-              </div>
-            </div>
-          )}
-        </div>
-      </modal.Component>
-
-      <article className="post">
-        <div className="header">
-          <div className="avatar">
-            {hideLogo ? (
-              <Mood mood={data.mood} />
-            ) : (
-              <Image
-                width={48}
-                height={48}
-                alt="Picture of the team"
-                src={data.team?.avatarUrl || ""}
-              />
-            )}
-          </div>
-          <div className="text">{data.team?.name}</div>
-          {!hideLogo && <Mood mood={data.mood} />}
-        </div>
-        <KPIs kpis={data.kpis?.slice(0, 3) || []}></KPIs>
-        <TabPanel>
-          <Tabs>
-            <Tab disabled={!data.priorities.length}>Priorités</Tab>
-            <Tab disabled={!data.needs.length}>Besoins</Tab>
-            <Tab disabled={!data.term.length}>Échéances</Tab>
-          </Tabs>
-          <Panels>
-            <div className="markdown-body">
-              <Markdown remarkPlugins={[remarkGfm]}>{data.priorities}</Markdown>
-            </div>
-            <div className="markdown-body">
-              <Markdown remarkPlugins={[remarkGfm]}>{data.needs}</Markdown>
-            </div>
-            <div className="markdown-body">
-              <Markdown remarkPlugins={[remarkGfm]}>{data.term}</Markdown>
-            </div>
-          </Panels>
-        </TabPanel>
-        <div className="footer">
-          <div className="info">
-            {data.created_at && format(new Date(data.created_at), "dd/MM/yyyy")}
-            {" - "}
-            <a
-              target="_blank"
-              rel="noreferrer"
-              className="mr-2"
-              href={`https://github.com/${data.author}`}
-            >
-              {data.author}
-            </a>
-          </div>
-          {editable && (
-            <div className="actions divide-x">
-              <Button
-                title="editer"
-                iconId="fr-icon-draft-line"
-                priority="tertiary no outline"
-                linkProps={{
-                  href: `/${data.team_slug}/publish/${data.id}`,
-                }}
-              />
-              <Button
-                title="supprimer"
-                iconId="fr-icon-delete-line"
-                priority="tertiary no outline"
-                nativeButtonProps={modal.buttonProps}
-              />
-            </div>
-          )}
-        </div>
-      </article>
-    </>
+        )}
+      </div>
+    </article>
   );
 }
--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -1,4 +1,17 @@
+"use client";
+
+import { useState } from "react";
 import Post from "@/components/post";
+import { useRouter } from "next/navigation";
+import deletePost from "@/actions/delete-post";
+import Button from "@codegouvfr/react-dsfr/Button";
+import SpinnerIcon from "@/components/icons/spinner-icon";
+import { createModal } from "@codegouvfr/react-dsfr/Modal";
+
+const modal = createModal({
+  id: "delete-post",
+  isOpenedByDefault: false,
+});
 
 export default function Posts({
   posts,
@@ -9,11 +22,63 @@ export default function Posts({
   editable?: boolean;
   hideLogos?: boolean;
 }) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [postToDelete, setPostToDelete] = useState<string>();
+
+  async function handleDeletion() {
+    setIsLoading(true);
+    await deletePost(postToDelete);
+    setPostToDelete(undefined);
+    router.refresh();
+    setIsLoading(false);
+    modal.close();
+  }
+
+  function showModal(id?: string) {
+    if (id) {
+      setPostToDelete(id);
+      modal.open();
+    }
+  }
+
   return (
     <section className="posts">
+      <modal.Component title="Suppression">
+        <div className="relative">
+          <div className={`${isLoading ? "invisible" : ""}`}>
+            <p className="my-8">
+              Êtes-vous sur de vouloir supprimer cette publication ?
+            </p>
+            <Button
+              title="supprimer"
+              priority="primary"
+              onClick={handleDeletion}
+              iconId="fr-icon-delete-line"
+              // TODO: make this button redish
+              // className="bg-[#c9191e] dark:bg-[#f95c5e] dark:text-[#2b1919] text-[#fef4f4] hover:dark:text-[#f95c5e] hover:dark:bg-[#f95c5e] hover:text-[#c9191e] hover:bg-[#c9191e]"
+            >
+              Supprimer
+            </Button>
+          </div>
+          {isLoading && (
+            <div className="absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center">
+              <div className="animate-spin w-24 h-24">
+                <SpinnerIcon />
+              </div>
+            </div>
+          )}
+        </div>
+      </modal.Component>
       {posts &&
         posts.map((post, i) => (
-          <Post key={i} data={post} editable={editable} hideLogo={hideLogos} />
+          <Post
+            key={i}
+            data={post}
+            editable={editable}
+            hideLogo={hideLogos}
+            onDeletion={showModal}
+          />
         ))}
     </section>
   );
