a728a02231b5164b6027e1372dd36716447934d9
Adrien Chauve <adrien@chauve.eu>
2024-03-18 16:01:47 +0100

    fix: Reduce sentry transaction sampling (#435)

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -9,7 +9,7 @@ Sentry.init({
   dsn: "https://c96c9319e5654102b38b750ecba85712@sentry.fabrique.social.gouv.fr/101",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
