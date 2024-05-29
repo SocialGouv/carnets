d5e53ca8240c3d8e7e8f42a4ade7007ccda07187
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2024-05-29 16:19:46 +0200

    fix: add logs

--- src/app/api/vault/route.ts
+++ src/app/api/vault/route.ts
@@ -7,9 +7,11 @@ var options = {
 
 export async function GET() {
   var vault = Vault(options);
+  console.log("VAULT", vault);
 
   try {
     const result = await vault.init({ secret_shares: 1, secret_threshold: 1 });
+    console.log("RESULT", result);
     var keys = result.keys;
     vault.token = result.root_token;
     const secrets = vault.unseal({ secret_shares: 1, key: keys[0] });
