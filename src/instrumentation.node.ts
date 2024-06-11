0a22fb75c17b068665ede3213b674e5a48db1a52
Adrien Chauve <adrien@chauve.eu>
2024-06-11 14:38:44 +0200

    fix: add missing sentry otel instrumentation (#447)

--- src/instrumentation.node.ts
+++ src/instrumentation.node.ts
@@ -1,9 +1,10 @@
-import { trace, context } from "@opentelemetry/api";
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { Resource } from "@opentelemetry/resources";
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { SEMRESATTRS_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
+
+import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
+import { NetInstrumentation } from "@opentelemetry/instrumentation-net";
+
 import {
   SentrySpanProcessor,
   SentryPropagator,
@@ -11,11 +12,13 @@ import {
 
 const sdk = new NodeSDK({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "next-app",
+    [SEMRESATTRS_SERVICE_NAME]: "next-app",
   }),
   // Sentry config
   spanProcessor: new SentrySpanProcessor(),
   textMapPropagator: new SentryPropagator(),
+
+  instrumentations: [new HttpInstrumentation(), new NetInstrumentation()],
 });
 
 sdk.start();
